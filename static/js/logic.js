5e63c99bd4ed8f2c4d8ac2d713f86a878ce53cf0
gretelup <gretelup@gmail.com>
2021-07-17 11:41:03 -0400

    Finish citibike map

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,46 +3,60 @@
 
 // Create the createMap function.
 
-function createMap(bikeStations){
+function createMap(bikeLayer) {
 
 
   // Create the tile layer that will be the background of our map.
-
+  let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
 
   // Create a baseMaps object to hold the lightmap layer.
-
+  let baseMaps = {
+    "Street Map": streetmap
+  }
 
   // Create an overlayMaps object to hold the bikeStations layer.
-
+  let overlayMaps = {
+    "Bike Stations": bikeLayer
+  }
 
   // Create the map object with options.
-
-
+  let map = L.map("map-id", {
+    center: [40.73, -74.0059],
+    zoom: 12,
+    layers: [streetmap, bikeLayer]
+  })
   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
 }
 // Create the createMarkers function.
-function createMarkers(response){
+function createMarkers(response) {
 
   // Pull the "stations" property from response.data.
-  console.log(response);
-
   // stations is an array of station objects
   let stations = response.data.stations;
+  console.log(stations);
 
   // Initialize an array to hold the bike markers.
-  let bikeMarkers = []
+  let bikeMarkers = [];
 
   // Loop through the stations array.
   stations.forEach(station => {
+
     // For each station, create a marker, and bind a popup with the station's name.
-    bikeMarkers.push(
-    // Add the marker to the bikeMarkers array.
-      L.marker([station.lat, station.lon])
+    let bikeMarker = L.marker([station.lat, station.lon])
       .bindPopup(`Station Name: ${station.name}`)
-    );
+
+    // Add the marker to the bikeMarkers array.
+    bikeMarkers.push(bikeMarker);
   })
   // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-  console.log(bikeMarkers);
+  let bikeLayer = L.layerGroup(bikeMarkers);
+
+  createMap(bikeLayer);
 }
 
 // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
