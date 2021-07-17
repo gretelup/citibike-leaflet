62d49f4d7f9bb03548e1f9209738c788e6b9a7b1
gretelup <gretelup@gmail.com>
2021-07-17 11:23:21 -0400

    Finish CreateMarkers Function

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,6 +3,8 @@
 
 // Create the createMap function.
 
+function createMap(bikeStations){
+
 
   // Create the tile layer that will be the background of our map.
 
@@ -17,19 +19,31 @@
 
 
   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-
+}
 // Create the createMarkers function.
+function createMarkers(response){
 
   // Pull the "stations" property from response.data.
+  console.log(response);
+
+  // stations is an array of station objects
+  let stations = response.data.stations;
 
   // Initialize an array to hold the bike markers.
+  let bikeMarkers = []
 
   // Loop through the stations array.
+  stations.forEach(station => {
     // For each station, create a marker, and bind a popup with the station's name.
-
+    bikeMarkers.push(
     // Add the marker to the bikeMarkers array.
-
+      L.marker([station.lat, station.lon])
+      .bindPopup(`Station Name: ${station.name}`)
+    );
+  })
   // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-
+  console.log(bikeMarkers);
+}
 
 // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
+d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(createMarkers);
